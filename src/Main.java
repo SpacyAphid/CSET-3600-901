c9d3b1085decf9fd945d0b893b4bc3c5d162951d
SpacyAphid <111330426+SpacyAphid@users.noreply.github.com>
2026-09-14 16:50:58 -0400

    Adding code to the repository

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println("i = " + i);
+        }
+    }
+}
